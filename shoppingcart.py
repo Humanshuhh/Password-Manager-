f0048ab15607fddc0de7fb99a18543757d44d73d
PRIYANSHU DEV <167692595+Humanshuhh@users.noreply.github.com>
2026-07-05 18:34:23 +0530

    Delete shoppingcart.py

--- shoppingcart.py
+++
@@ -1,7 +0,0 @@
-item = input("what would you like to take ");
-price = float(input("what is the price of the item"))
-quantity = int(input("how much do you want"));
-
-totalcost = price * quantity;
-
-print(f"your total bill is {totalcost}")

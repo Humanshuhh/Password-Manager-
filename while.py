22e71dab3e00b7cb6a029e4df650f723f46808a7
PRIYANSHU DEV <167692595+Humanshuhh@users.noreply.github.com>
2026-07-05 18:33:34 +0530

    Delete while.py

--- while.py
+++
@@ -1,7 +0,0 @@
-num = int(input("enter an number between 1 - 10"))
-
-while num < 1 or num > 10 :
-    print(f"your number {num} is not valid");
-    num = int(input("enter an number between 1 - 10"))
-
-    print(f"Your number is {num}")
